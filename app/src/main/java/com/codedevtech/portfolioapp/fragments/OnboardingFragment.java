6cf01d67c758ea3c580472f9c999231900cfd7a1
Nathany Natornam Attipoe <attipoen@itconsortiumgh.com>
2019-09-09 17:44:54 +0000

    init

---
+++ app/src/main/java/com/codedevtech/portfolioapp/fragments/OnboardingFragment.java
@@ -0,0 +1,32 @@
+package com.codedevtech.portfolioapp.fragments;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.codedevtech.portfolioapp.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class OnboardingFragment extends Fragment {
+
+
+    public OnboardingFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_onboarding, container, false);
+    }
+
+}
