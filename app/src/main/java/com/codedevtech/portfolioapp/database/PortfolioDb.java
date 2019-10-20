ed6de3da61270ba1084f6ac0796089910b93175b
Nathany Attipoe <kobbatt@gmail.com>
2019-10-20 01:42:02 +0000

    exportSchema rules added, default set to false

--- app/src/main/java/com/codedevtech/portfolioapp/database/PortfolioDb.java
+++ app/src/main/java/com/codedevtech/portfolioapp/database/PortfolioDb.java
@@ -6,7 +6,8 @@ import androidx.room.RoomDatabase;
 import com.codedevtech.portfolioapp.database.dao.UserDao;
 import com.codedevtech.portfolioapp.models.User;
 
-@Database(entities = {User.class}, version = 1)
+//exportschema should be true for non production builds
+@Database(entities = {User.class}, version = 1, exportSchema = false)
 public abstract class PortfolioDb extends RoomDatabase {
 
     public abstract UserDao userDao();
